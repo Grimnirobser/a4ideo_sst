0d07003e4d4962e0e0661b40962270d11c5d2641
Grimnirobser <grimnirobser@gmail.com>
2024-03-02 20:08:46 -0500

    update update-env.sh

--- update-env.sh
+++ update-env.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "DATABASE_URL=$(aws ssm get-parameter --name 'a4ideo_database' --with-decryption | jq '.Parameter.Value')" >> .env
+echo "DATABASE_URL=$(aws ssm get-parameter --name arn:aws:ssm:us-east-2:975050345920:parameter/a4ideo_database --with-decryption | jq '.Parameter.Value')" >> .env
