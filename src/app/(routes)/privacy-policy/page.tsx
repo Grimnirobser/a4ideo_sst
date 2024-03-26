6d2823d5f156fb83f64ec88c4d1f11c78543e507
Grimnirobser <grimnirobser@gmail.com>
2024-03-26 00:19:29 -0400

    update privacy policy and terms

--- src/app/(routes)/privacy-policy/page.tsx
+++ src/app/(routes)/privacy-policy/page.tsx
@@ -1,19 +1,95 @@
-import  TermlyScriptWrapper  from '@/components/TermlyScriptWrapper';
 
 
+const PrivacyPolicy = () => {
 
-const Page = () => {
     return (
+        <div className="flex flex-col items-center h-full bg-slate-100 mx-60">
+            <title> Privacy Policy</title>
+            <h1 className="text-4xl text-gray-700 font-bold mt-4">Privacy Policy</h1>
 
-        <div>
-            <title>Privacy Policy</title>
-            <h1>Privacy Policy</h1>
+            <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg">
+            Last updated: [03/26/2024]
 
-            <TermlyScriptWrapper dataId='6e8d6c0f-88d3-4548-a200-1e4624009a54' />
+            </p>
+
+            <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">
+At A4ideo Inc. (&quot;A4ideo,&quot; &quot;we,&quot; &quot;us,&quot; or &quot;our&quot;), we are committed to protecting the privacy of our users. This Privacy Policy outlines how we collect, use, disclose, and safeguard your personal information when you visit our website, A4ideo (the &quot;Website&quot;). By accessing or using our Website, you consent to the practices described in this Privacy Policy.
+            </p>
+
+
+<p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">1. Information We Collect</p>
+
+<p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">
+
+   a. Personal Information: When you register an account, sign up for our newsletter, or contact us through our Website, we may collect personal information such as your name, email address, phone number, and other contact details.
+   b. Usage Information: We may collect information about how you interact with our Website, including your IP address, browser type, referring/exit pages, and operating system.
+   c. Cookies and Similar Technologies: We may use cookies, web beacons, and similar technologies to enhance your experience, analyze trends, and gather demographic information.
+   </p>
+
+   <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">2. Use of Information</p>
+   <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">
+
+   a. We use the information we collect to provide, maintain, and improve our Website and services.
+   b. We may use your personal information to respond to your inquiries, send you newsletters or promotional materials, and communicate with you about our services.
+   c. We may use aggregated and anonymized data for statistical analysis and to improve our Website and services.
+   </p>
+
+   <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">3. Disclosure of Information</p>
+   <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">
+   a. We may share your personal information with trusted third-party service providers who assist us in operating our Website and conducting our business.
+   b. We may disclose your information if required to do so by law or if we believe that such action is necessary to comply with legal obligations, protect our rights, or ensure the safety of our users.
+   c. In the event of a merger, acquisition, or sale of all or a portion of our assets, your information may be transferred to the acquiring entity.
+   </p>
+
+
+   <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">4. Data Security</p>
+
+   <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">
+   We implement reasonable security measures to protect your personal information from unauthorized access, alteration, disclosure, or destruction. However, no method of transmission over the Internet or electronic storage is 100% secure, and we cannot guarantee absolute security.
+   </p>
+
+   <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">5. Third-Party Links</p>
+
+   <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">
+   Our Website may contain links to third-party websites. We are not responsible for the privacy practices or content of those websites. We encourage you to review the privacy policies of any third-party websites you visit.
+   </p>
+
+   <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">6. Children&apos;s Privacy</p>
+
+   <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">
+   Our Website is not intended for children under the age of 13. We do not knowingly collect personal information from children. If we become aware that we have collected personal information from a child without parental consent, we will take steps to remove that information from our servers.
+   </p>
+
+   <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">7. Your Choices</p>
+
+   <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">
+   a. You may opt-out of receiving promotional emails from us by following the unsubscribe instructions provided in those emails.
+   b. You may update or correct your personal information by contacting us at the email address provided below.
+   </p>
+
+   <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">8. Changes to this Privacy Policy</p>
+
+   <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">
+   We may update this Privacy Policy from time to time. The most current version will be posted on our Website with the effective date. We encourage you to review this Privacy Policy periodically for any changes.
+   </p>
+
+   <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">9. Contact Us</p>
+
+   <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">
+   If you have any questions, concerns, or requests regarding this Privacy Policy or our privacy practices, please contact us at contact@a4ideo.com.
+   </p>
+
+   <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">10. Acknowledgement</p>
+
+   <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6 mb-20">
+   BY USING SERVICE OR OTHER SERVICES PROVIDED BY US, YOU ACKNOWLEDGE THAT YOU HAVE READ THESE TERMS OF SERVICE AND AGREE TO BE BOUND BY THEM.
+
+            </p>
+
         </div>
-    )
+        );
 
 
 }
 
-export default Page;
+export default PrivacyPolicy;
