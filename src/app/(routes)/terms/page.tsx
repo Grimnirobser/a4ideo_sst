52c2a99b0f5fa2a9d1ad6ab6f53b1a90789261b1
Grimnirobser <grimnirobser@gmail.com>
2024-03-26 00:19:29 -0400

    update privacy policy and terms

--- src/app/(routes)/terms/page.tsx
+++ src/app/(routes)/terms/page.tsx
@@ -1,21 +1,103 @@
 
 
-
-const Page = () => {
-
+const Terms = () => {
 
     return (
+        <div className="flex flex-col items-center h-full bg-slate-100 mx-60">
+            <title>Terms of Services</title>
+            <h1 className="text-4xl text-gray-700 font-bold mt-4">Terms of Service</h1>
 
-        <div>
-            <title>Terms of Service</title>
-            <h1>Terms of Service</h1>
+            <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg">
+            Last updated: [03/26/2024]
+
+            </p>
+
+            <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">
+            Welcome to A4ideo, a website operated by A4ideo Inc. (&quot;Company,&quot; &quot;we,&quot; &quot;us,&quot; or &quot;our&quot;). These Terms of Service (&quot;Terms&quot;) govern your access to and use of our website, services, and content (collectively, the &quot;Services&quot;). By accessing or using our Services, you agree to be bound by these Terms. If you do not agree to these Terms, do not use our Services.
+             </p>
+
+
+<p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">1. Acceptance of Terms</p>
+
+<p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">
+By using our Services, you represent that you are at least 13 years old and have the legal capacity to enter into these Terms.
+If you are under 18, you represent that you have your parent or guardian’s permission to use the Service. Please have them read this Agreement with you.
+If you are a parent or legal guardian of a user under the age of 18, by allowing your child to use the Service, you are subject to the terms of this Agreement and responsible for your child’s activity on the Service. 
+If you are using our Services on behalf of an organization, you represent that you have the authority to bind that organization to these Terms.
+   </p>
+
+   <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">2. User Accounts</p>
+   <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">
+   To access certain features of our Services, you may need to create an account. You agree to provide accurate, current, and complete information during the registration process and to update such information as necessary. You are responsible for maintaining the confidentiality of your account credentials and for all activities that occur under your account.
+   </p>
+
+   <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">3. Intellectual Property</p>
+   <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">
+   All content and materials available through our Services, including but not limited to text, graphics, logos, images, and software, are the property of A4ideo Inc. or its licensors and are protected by intellectual property laws. You may not use, reproduce, modify, or distribute any of our content without our prior written consent.
+   </p>
+
+
+   <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">4. User Content</p>
+
+   <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">
+   By submitting, posting, or displaying content on or through our Services, you grant us a worldwide, non-exclusive, royalty-free license to use, reproduce, modify, adapt, publish, translate, distribute, and display such content for the purpose of providing and improving our Services.
+   </p>
+
+   <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">5. Prohibited Conduct</p>
+
+   <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">
+   When using our Services, you agree not to: 
+    a. Violate any applicable laws or regulations.
+    b. Infringe upon the intellectual property rights of others.
+    c. Engage in any fraudulent, deceptive, or misleading activities.
+    d. Interfere with or disrupt the integrity or performance of our Services.
+    e. Attempt to gain unauthorized access to our Services or systems.
+   </p>
+
+   <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">6. Third-Party Services</p>
+
+   <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">
+   Our Services may contain links to third-party websites or services. We do not endorse or assume any responsibility for the content, privacy policies, or practices of any third-party websites or services. Your interactions with these third parties are solely between you and the third party.
+   </p>
+
+   <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">7. Termination</p>
+
+   <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">
+   We reserve the right to suspend or terminate your access to our Services at any time, without notice, for any reason, including if we reasonably believe you have violated these Terms.
+   </p>
+
+   <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">8. Disclaimer of Warranties</p>
+
+   <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">
+   Our Services are provided on an &quot;as is&quot; and &quot;as available&quot; basis. We make no representations or warranties of any kind, express or implied, regarding the operation of our Services or the information, content, materials, or products included therein.
+   </p>
+
+   <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">9. Limitation of Liability</p>
+
+   <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">
+   In no event shall A4ideo Inc. be liable for any indirect, incidental, special, consequential, or punitive damages arising out of or related to your use of our Services.
+   </p>
+
+   <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">10. Governing Law</p>
+
+   <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">
+   These Terms shall be governed by and construed in accordance with the Ontario laws. Any legal action or proceeding arising out of or related to these Terms shall be brought exclusively in the Ontario Court of Justice.
+    </p>
+
+    <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">11. Changes to Terms</p>
+    <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6">
+    We reserve the right to modify these Terms at any time. Your continued use of our Services after any changes constitutes your acceptance of the new Terms.
+    </p>
+
+    <p className="text-gray-700 mt-4 text-left font-serif mx-4 text-lg indent-6 mb-20">
+
+    If you have any questions about these Terms, please contact us at contact@a4ideo.com.
+    </p>
 
         </div>
-        
-        )
-
-
+        );
 
 
 }
-export default Page;
+
+export default Terms;
