79fe8f823c9c6e7d8494a5969839c48d3371218d
Grimnirobser <grimnirobser@gmail.com>
2024-03-27 19:10:10 -0400

    add a no found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,37 @@
+import Link from 'next/link'
+import Image from 'next/image';
+import { buttonVariants} from '@/components/ui/button'
+import { ArrowRight } from 'lucide-react'
+
+
+
+export default async function NotFoundPage() {
+
+  return (
+
+      <div className='flex flex-col items-center text-center mx-auto justify-center space-y-6 sm:w-[350px] lg:px-0'>  
+        <Image
+            unoptimized
+            fill
+            sizes="100%"
+            className='-z-10 object-contain object-center rounded-lg h-20 w-20'
+            src="./rabbit-hole.jpeg"
+            alt='rabbit hole'
+        />
+
+        <Link
+          className={buttonVariants({
+            variant: 'link',
+            className: 'absolute top-10 right-10 gap-1.5 text-lg',
+          })}
+          href='/'>
+          Go back to home
+          <ArrowRight className='h-4 w-4' />
+        </Link>
+      </div>
+
+
+    )
+}   
+  
+
