e7ea0281dcf05b79dc24422c6f33842670b3d5c8
Grimnirobser <grimnirobser@gmail.com>
2024-03-27 19:10:10 -0400

    add a no found page

---
+++ src/app/(routes)/studio/edit-problemset/[problemsetId]/page.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import getChannelById from "@/actions/getChannelById";
+import getCommentsByVideoId from "@/actions/getCommentsByVideoId";
+import increaseVideoViewCount from "@/actions/increaseVideoViewCount";
+import CommentSection from "@/components/video/CommentSection/CommentSection";
+import Description from "@/components/video/Description";
+import LikeSubscribeSection from "@/components/video/LikeSubscribeSection/LikeSubscribeSection";
+import VideoPlayer from "@/components/video/VideoPlayer";
+import YoutubePlayer from "@/components/video/YoutubePlayer";
+import getProblemsetsByVideoId from "@/actions/getProblemsetsByVideoId";
+import ProblemsetSection from "@/components/video/ProblemsetSection/ProblemsetSection";
+import { DocView } from "@/components/video/DocView";
+
+interface VideoPageParams {
+  problemsetId: string;
+}
+
+export default function EditProblemsetPage({
+  params,
+}: {
+  params: VideoPageParams;
+}) {
+//   const { videoId } = params;
+//   const problemsets = await getProblemsetsByVideoId({ videoId });
+//   const video = await increaseVideoViewCount({ videoId });
+//   const channel = await getChannelById({ channelId: video?.channelId });
+//   const comments = await getCommentsByVideoId({ videoId,});
+
+  return video && channel && comments ? (
+    <div className="flex flex-col lg:flex-row mx-6 mt-2 gap-4">
+      <div className="w-full lg:w-4/6 flex flex-col gap-4 overflow-y-auto max-h-screen [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
+        {/* <VideoPlayer videoSrc={video.videoSrc} /> */}
+        {video.youtubeId === "" ? <DocView imageSrc={video.thumbnailSrc}/> : <YoutubePlayer youtubeId={video.youtubeId}/>}
+        <h1 className="text-2xl font-medium break-words hyphens-auto">{video.title}</h1>
+        <Description video={video} />
+      </div>
+
+      <div className="w-full lg:w-2/6 max-h-screen overflow-y-auto no-scrollbar">
+        <ProblemsetSection problemsets={problemsets!} videoId={video.id} />
+      </div>
+      
+    </div>
+  ) : (
+    <h1>Video not found</h1>
+  );
+}
