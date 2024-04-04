a1d899cd1d0f6a3a3355e663e1905210f6256912
Grimnirobser <grimnirobser@gmail.com>
2024-04-04 18:05:23 -0400

    add some banner in the first page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import getTrendingProblemsets from "@/actions/getTrendingProblemsets";
 import HomePageProblemset from "@/components/shared/HomePageProblemset";
+import FirstPageBanner from "@/components/shared/FirstPageBanner";
 
 export default async function Home() {
   const trendingProblemsets = await getTrendingProblemsets();
 
   return (
+    <>
+    <FirstPageBanner bgColor="red"/>
+    <div className="mx-10 sm:mx-20 text-4xl flex flex-col gap-6">
+      <p><a className="underline decoration-double decoration-rose-500">Watch</a>{' '} & {' '}<a className="underline decoration-double decoration-green-500">Task</a>{' '}&{' '}<a className="underline decoration-double decoration-sky-500">Feedback</a></p>
+
+    </div>
     <div className="mx-10 sm:mx-20 py-8 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 gap-4">
       {trendingProblemsets
         ? trendingProblemsets.map((trendingProblemset) => {
@@ -17,5 +24,6 @@ export default async function Home() {
           })
         : "No problemsets found"}
     </div>
+  </>
   );
 }
--- src/components/video/DocView.tsx
+++ src/components/video/DocView.tsx
@@ -10,14 +10,7 @@ export const DocView = ({documentSrc}: DocViewProps) => {
     return (
 
         <div className="relative w-full flex justify-center m-auto group bg-white aspect-w-2 aspect-h-3 overflow-y-auto no-scrollbar max-h-screen">
-            {/* <Image
-                unoptimized
-                alt="Avatar"
-                className="w-full z-[5] rounded-xl"
-                src={imageSrc}
-                height={150}
-                width={100}
-            /> */}
+
             <embed
                 className="w-full h-full rounded-xl z-[5]"
                 style={{
